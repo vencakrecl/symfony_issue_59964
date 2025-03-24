eff3b734125d9a3727d476f9fc110691865afc3e
Venca Krecl <venca.email@gmail.com>
2025-03-24 20:01:05 +0100

    chore: add serializer bug

---
+++ src/Command/BugCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+#[AsCommand('serializer:bug')]
+class BugCommand extends Command
+{
+	public function __construct(private readonly NormalizerInterface $serializer)
+	{
+		parent::__construct(null);
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output): int
+	{
+		$class = new class
+		{
+			public string $camelCase = 'value';
+		};
+
+		var_dump($this->serializer->normalize($class, 'json'));
+		var_dump($this->serializer->normalize(['camelCase' => 'value'], 'json'));
+
+		return self::SUCCESS;
+	}
+}
